ca83842d249da73794cbcafb926865ee8f020e9c
dh1010a <dh1010a@naver.com>
2026-02-24 16:25:30 +0900

    feat: 요구사항 3 구현

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
 ```
 ./gradlew test
 ```
+
+## 요구사항 2
+
+### bash 실행 방법
+```
+./gradlew test
+```
+
+## 요구사항 3
+
+### bash 실행 방법
+```
+./gradlew cucumberTest
+```
--- src/test/java/gift/acceptance/CucumberSpringConfiguration.java
+++ src/test/java/gift/acceptance/CucumberSpringConfiguration.java
@@ -6,24 +6,21 @@ import io.cucumber.spring.CucumberContextConfiguration;
 import io.restassured.RestAssured;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ActiveProfiles;
 
 @CucumberContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("cucumber")
 public class CucumberSpringConfiguration {
-
-    @LocalServerPort
-    int port;
 
     @Autowired
     JdbcTemplate jdbcTemplate;
 
     @Before
     public void setUp() {
-        RestAssured.port = port;
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = 28080;
         jdbcTemplate.execute("TRUNCATE TABLE wish, option, product, category, member CASCADE");
         CommonStepDefinitions.reset();
     }
